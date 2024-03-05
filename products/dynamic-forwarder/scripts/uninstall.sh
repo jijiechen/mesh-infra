9a812939dc9961224e6396483c124d153cd97091
Jay Jijie Chen <1180092+jijiechen@users.noreply.github.com>
2024-03-05 13:10:12 +0800

    Update uninstall.sh

--- products/dynamic-forwarder/scripts/uninstall.sh
+++ products/dynamic-forwarder/scripts/uninstall.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-crontab -l 2>/dev/null | grep -v refresh-sni-proxy | crontab -
-
 systemctl stop envoy.service
 systemctl disable envoy.service
 rm -f /etc/systemd/system/envoy.service
